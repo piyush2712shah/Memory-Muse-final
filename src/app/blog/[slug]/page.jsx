0f74cb238b1c1f5e4d58027a725f6300b72ab1ac
piyush2712shah <164843354+piyush2712shah@users.noreply.github.com>
2024-07-24 16:34:28 +0545

    solved local host issues

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -6,7 +6,7 @@ import { getPost } from "@/lib/data";
 
 // FETCH DATA WITH AN API
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/blog/${slug}`);
+  const res = await fetch(`https://memorymuse-git-main-piyush-shahs-projects-9cf32da7.vercel.app/api/blog/${slug}`);
 
   if (!res.ok) {
     throw new Error("Something went wrong");
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import { getPosts } from "@/lib/data";
 
 // FETCH DATA WITH AN API
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+  const res = await fetch("https://memorymuse-git-main-piyush-shahs-projects-9cf32da7.vercel.app/api/blog", {next:{revalidate:3600}});
 
   if (!res.ok) {
     throw new Error("Something went wrong");
